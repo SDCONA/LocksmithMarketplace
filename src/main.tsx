120a7eaa8d04893fc965766cef82ec98e1903cb0
Samvel Stepanian <parlando87@gmail.com>
2026-01-08 12:04:31 -0500

    global css fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 // Correct Tailwind CSS entry
-import './index.css'
+import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
